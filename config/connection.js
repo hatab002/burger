176801a99db90ba531f4cdab34f0e2d15329087f
hatab002 <hatab002@umn.edu>
2018-06-26 11:39:56 -0500

    JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {connection = mysql.createConnection({
     port: 3306,
     host: "localhost",
     user: "root",
     password: "",
-    database: "burgers_db"
-});
+    database: "burgers_db"})
+};
 
 connection.connect(function(err){
     if (err){
